7d7a66681c0d0f785337bb1e8abbe93defa2443d
JLPassos <jlpassos@hotmail.com>
2024-05-30 07:34:34 -0300

    Confirmado

---
+++ tarefas.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerenciador de Tarefas Diárias</title>
+</head>
+<body>
+    <h1>Gerenciador de Tarefas</h1>
+    <form> 
+        <fieldset> 
+            <legend>Nova tarefa</legend> 
+            <label> Tarefa: <input type="text" name="nome" /> </label> 
+            <input type="submit" value="Cadastrar" /> 
+        </fieldset> 
+    </form>
+</body>
+</html
